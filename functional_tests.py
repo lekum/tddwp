fe2ebf077c53fd0c6e84aa538305fe1dadd4646c
Alejandro Guirao <lekumberri@gmail.com>
2014-06-23 09:37:14 +0200

    Functional tests now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,25 +17,40 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title) #
-        self.fail('Finish the test!') #
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
 
-        # She types "Buy peacock feathers" into a text box (Edith's hobby 
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the pages lists "1: Buy
         # peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
 
-        # There is still a textbox inviting her to add another item. 
-        # She enters "Use peacock feathers to make a fly" 
+        table = self.browser.find_elements_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # There is still a textbox inviting her to add another item.
+        # She enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
 
-        # Edith wonders wheter the site will remember her list. 
-        # The she sees that the site has generated a unique URL for 
+        # Edith wonders wheter the site will remember her list.
+        # The she sees that the site has generated a unique URL for
         # her -- there is some explanatory texto to that effect
 
         # She visits that URL - her to-do list is still there.
