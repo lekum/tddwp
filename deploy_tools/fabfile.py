d838fb62414c396d49c742f5427f41eb854a6664
Alejandro Guirao <lekumberri@gmail.com>
2014-07-28 13:29:14 +0200

    Added tasks to fabfile

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,24 +1,25 @@
 from fabric.contrib.files import append, exists, sed
-from fabric.api import env, local, run
+from fabric.api import env, local, run, task
 import random
 
 REPO_URL = 'https://github.com/lekum/tddwp'
 
+@task
 def deploy():
     site_folder = '/home/%s/sites/%s' % (env.user, env.host)
     source_folder = site_folder + '/source'
-    _create_directory_structure_if_necessary(site_folder)
-    _get_latest_source(source_folder)
-    _update_settings(source_folder, env.host)
-    _update_virtualenv(source_folder)
-    _update_static_files(source_folder)
-    _update_database(source_folder)
+    create_directory_structure_if_necessary(site_folder)
+    get_latest_source(source_folder)
+    update_settings(source_folder, env.host)
+    update_virtualenv(source_folder)
+    update_static_files(source_folder)
+    update_database(source_folder)
 
-def _create_directory_structure_if_necessary(site_folder):
+def create_directory_structure_if_necessary(site_folder):
     for subfolder in ('database', 'static', 'virtualenv', 'source'):
         run('mkdir -p %s/%s' % (site_folder, subfolder))
 
-def _get_latest_source(source_folder):
+def get_latest_source(source_folder):
     if exists(source_folder + '/.git'):
         run('cd %s && git fetch' % (source_folder,))
     else:
@@ -26,7 +27,7 @@ def _get_latest_source(source_folder):
     current_commit = local("git log -n 1 --format=%H", capture=True)
     run('cd %s && git reset --hard %s' % (source_folder, current_commit))
 
-def _update_settings(source_folder, site_name):
+def update_settings(source_folder, site_name):
     settings_path = source_folder + '/superlists/settings.py'
     sed(settings_path, "DEBUG = True", "DEBUG = False")
     sed(settings_path,
@@ -40,15 +41,15 @@ def _update_settings(source_folder, site_name):
         append(secret_key_file, "SECRET_KEY = '%s'" % (key,))
     append(settings_path, '\nfrom .secret_key import SECRET_KEY')
 
-def _update_virtualenv(source_folder):
+def update_virtualenv(source_folder):
     virtualenv_folder = source_folder + '/../virtualenv/'
     if not exists(virtualenv_folder + '/bin/pip'):
         run('virtualenv --python=python3 %s' % (virtualenv_folder,))
 
-def _update_static_files(source_folder):
+def update_static_files(source_folder):
     run('cd %s && ../virtualenv/bin/python manage.py collectstatic --noinput' %
         (source_folder,))
 
-def _update_database(source_folder):
+def update_database(source_folder):
     run('cd %s %% ../virtualenv/bin/python manage.py migrate --noinput' %
         (source_folder,))
