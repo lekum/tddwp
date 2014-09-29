35a24943987ff8aceec1aabdbdfc00d4dfb166bf
Alejandro Guirao <lekumberri@gmail.com>
2014-09-29 15:30:56 +0200

    First implementation of qunit Persona tests

---
+++ accounts/static/accounts.js
@@ -0,0 +1,12 @@
+var initialize = function (navigator) {
+    $('#id_login').on('click', function () {
+        navigator.id.request();
+    });
+    navigator.id.watch();
+};
+
+window.Superlists = {
+    Accounts: {
+        initialize: initialize 
+    }
+};
