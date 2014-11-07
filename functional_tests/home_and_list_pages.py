66e8ebff0eff755f5cbf7043ef2f397a8a375352
Alejandro Guirao <lekumberri@gmail.com>
2014-11-07 13:51:34 +0100

    Fix typos in tests

--- functional_tests/home_and_list_pages.py
+++ functional_tests/home_and_list_pages.py
@@ -29,7 +29,7 @@ class HomePage(object):
     def go_to_my_lists_page(self):
         self.test.browser.find_element_by_link_text('My lists').click()
         self.test.wait_for(lambda: self.test.assertEqual(
-            self.test.browser.find_element_by_tag_name('h1').test,
+            self.test.browser.find_element_by_tag_name('h1').text,
             'My lists'
         ))
 
